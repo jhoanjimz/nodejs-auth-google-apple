6f0b53ba98887ca953a04575ff987b75a19f1887
jhoanjimz <jhoan.developer@gmail.com>
2023-09-09 12:09:42 -0500

    Apple SignIn sin probar

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
 const { response } = require('express');
+const AppleAuth = require("apple-auth");
+const jwt = require("jsonwebtoken");
 
 const { validarGoogleIdToken } = require('../helpers/google-verify-token');
 
@@ -30,6 +33,60 @@ const googleAuth = async ( req, res = response ) => {
 
 }
 
+const callbackApple = async (req, res = response) => {
+    const redirect = `intent://callback?${new URLSearchParams(
+      request.body
+    ).toString()}#Intent;package=${
+      process.env.ANDROID_PACKAGE_IDENTIFIER
+    };scheme=signinwithapple;end`;
+  
+    console.log(`Redirecting to ${redirect}`);
+  
+    response.redirect(307, redirect);
+}
+
+const signInWithApple = async (request, response) => {
+    const auth = new AppleAuth(
+      {
+        // use the bundle ID as client ID for native apps, else use the service ID for web-auth flows
+        // https://forums.developer.apple.com/thread/118135
+        client_id:
+          request.query.useBundleId === "true"
+            ? process.env.BUNDLE_ID
+            : process.env.SERVICE_ID,
+        team_id: process.env.TEAM_ID,
+        redirect_uri:
+          "https://app-google-apple-signin-0f15519ad111.herokuapp.com/callbacks/sign_in_with_apple", // does not matter here, as this is already the callback that verifies the token after the redirection
+        key_id: process.env.KEY_ID,
+      },
+      fs.readFileSync('../keys/keysignin.p8').toString(),
+      "text"
+    );
+  
+    console.log(request.query);
+  
+    const accessToken = await auth.accessToken(request.query.code);
+  
+    const idToken = jwt.decode(accessToken.id_token);
+  
+    const userID = idToken.sub;
+  
+    console.log(idToken);
+  
+    // `userEmail` and `userName` will only be provided for the initial authorization with your app
+    const userEmail = idToken.email;
+    const userName = `${request.query.firstName} ${request.query.lastName}`;
+  
+    // 👷🏻‍♀️ TODO: Use the values provided create a new session for the user in your system
+    const sessionID = `NEW SESSION ID for ${userID} / ${userEmail} / ${userName}`;
+  
+    console.log(`sessionID = ${sessionID}`);
+  
+    response.json({ sessionId: sessionID });
+}
+
 module.exports = {
-    googleAuth
+    googleAuth,
+    callbackApple,
+    signInWithApple
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
-const { googleAuth } = require('../controllers/auth');
+const { googleAuth, callbackApple, signInWithApple } = require('../controllers/auth');
 
 const router = Router();
 
 
 router.post('/google', googleAuth);
 
+router.post("/callbacks/sign_in_with_apple", callbackApple);
 
+router.post("/sign_in_with_apple", signInWithApple);
 
 module.exports = router;
